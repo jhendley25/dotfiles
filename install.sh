30ddf961d7289f1a5de2171d23667795ddf5bf7e
jhendley25 <jake.hendley25@gmail.com>
2016-08-16 09:32:41 -0400

    work on install script

--- install.sh
+++ install.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
+# Update dotfiles itself first
+
+[ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
+
+# run the install scripts
+
+. "$DOTFILES_DIR/install/brew.sh"
+. "$DOTFILES_DIR/install/brew-cask.sh"
+. "$DOTFILES_DIR/install/rvm.sh"
+. "$DOTFILES_DIR/install/npm.sh"
+
 
 ln -svf ~/dotfiles/runcom/.zshrc ~/.zshrc
